c9e566e957f00402059ed49095f5b184569e27c7
Nichole De La Cruz <nicholed@zillowgroup.com>
2020-02-29 13:12:06 -0800

    Create stack and display services, tasks and containers running in the swarm

--- README.md
+++ README.md
@@ -1,5 +1,10 @@
-Build image: `docker build -t <username>/test-project .`
+# Test Project
+A dashboard-style application that allows the user to see all of the docker containers running in their own machine and other useful information about them.
 
-Start container:  `docker run -v /var/run/docker.sock:/var/run/docker.sock -p 4040:4040 <username>/test-project`.
+## Getting Started
+- Run `docker-compose up test-project`
+- Go to [localhost:4040](localhost:4040)
 
-Go to [localhost:4040](localhost:4040)
+## Deploy the Test Project to Swarm
+- Make sure that Swarm is enabled on your Docker Desktop by typing `docker system info`, and looking for a message `Swarm: active`. If Swarm isn’t running, simply type `docker swarm init` in a shell prompt to set it up.
+- Run `docker stack deploy demo`
--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const Docker = require('dockerode');
 
 const app = express();
-const PORT = 4040;
+const port = process.env.DOCK_PORT || 4040;
 
 const docker = new Docker()
 
@@ -12,10 +12,36 @@ app.get('/', async (req, res) => {
   try {
     const containers = await docker.listContainers()
     const runningContainers = containers.filter(c => c.State === 'running')
-    res.render('index', { title: 'Docker Containers', runningContainers });
+    const services = await docker.listServices()
+    const tasks = await docker.listTasks()
+
+    const swarm = services.reduce((acc, svc) => {
+      const replicas = svc.Mode && svc.Mode.Replicated && svc.Mode.Replicated.Replicas;
+      const taskCount = replicas ? { replicas } : {}
+      return [
+        ...acc,
+        {
+          ...taskCount,
+          id: svc.ID.slice(0, 12),
+          name: svc.Spec.Name,
+          tasks: tasks
+            .sort((a, b) => a.Slot - b.Slot)
+            .filter(t => t.ServiceID === svc.ID)
+            .map(t => ({
+              id: t.ID.slice(0, 12),
+              slot: t.Slot,
+              nodeId: t.NodeID.slice(0, 12),
+              container: t.Status,
+            })),
+        },
+      ];
+    }, [])
+
+    console.dir({ services }, { depth: null })
+    res.render('index', { title: 'Docker Containers', runningContainers, swarm });
   } catch (err) {
     console.error(err)
   }
 })
 
-app.listen(PORT, () => console.log(`📡 Server is listening on port ${PORT}!`));
+app.listen(port, () => console.log(`📡 Server is listening on port ${port}!`));
